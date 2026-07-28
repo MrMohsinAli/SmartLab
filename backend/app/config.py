808ae971d00777b78afb5cfa41988a28393f9635
MrMohsinAli <miamibeach1199@gmail.com>
2026-07-28 20:15:46 +0500

    Structured Medication Parser

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,17 +1,11 @@
 import os
 from dotenv import load_dotenv
 
-# Load variables from .env file
 load_dotenv()
 
 class Settings:
-    # Database
     DATABASE_URL: str = os.getenv("DATABASE_URL")
-    
-    # Google API Key (for OCR / Gemini)
     GOOGLE_API_KEY: str = os.getenv("GOOGLE_API_KEY")
-    
-    # OpenAI API
     OPENAI_API_KEY: str = os.getenv("OPENAI_API_KEY")
-
+    
 settings = Settings()
